a15ffe82374f2efe9a1c538659593d5fd78e0342
Erfan <erfan.ehsany@outlook.com>
2024-05-05 02:24:19 +0330

    fix test

--- app/users/tests/test_tasks.py
+++ app/users/tests/test_tasks.py
@@ -1,17 +1,17 @@
 import pytest
 from celery.result import EagerResult
 
-from app.users.tasks import get_users_count
+from app.users.tasks import email_users_count
 from app.users.tests.factories import UserFactory
 
 pytestmark = pytest.mark.django_db
 
 
 def test_user_count(settings):
-    """A basic test to execute the get_users_count Celery task."""
+    """A basic test to execute the email_users_count Celery task."""
     batch_size = 100
     UserFactory.create_batch(batch_size)
     settings.CELERY_TASK_ALWAYS_EAGER = True
-    task_result = get_users_count.delay()
+    task_result = email_users_count.delay()
     assert isinstance(task_result, EagerResult)
     assert task_result.result == batch_size
